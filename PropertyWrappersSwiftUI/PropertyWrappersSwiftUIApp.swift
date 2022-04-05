46c337c52b8c6112d5de7a90c18a2d19e469ca70
porohov <kombat_kos@mail.ru>
2022-04-05 15:40:50 +0400

    Initial Commit

---
+++ PropertyWrappersSwiftUI/PropertyWrappersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrappersSwiftUIApp.swift
+//  PropertyWrappersSwiftUI
+//
+//  Created by porohov on 05.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrappersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
